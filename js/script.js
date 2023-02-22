74d42a9040bb194f378cc66a39fb4bda688fcfdc
K-DoYeon <loveworud9@gmail.com>
2023-02-22 17:59:30 +0900

    0222

--- js/script.js
+++ js/script.js
@@ -32,4 +32,14 @@ $(function(){
         $('.tab-content>div').removeClass('active'); //tab-content의 모든 active를 지운다.
         $(myid).addClass('active'); // 가져온 data-idx 값과 같은 tab-content값에 active를 추가한다.
     })
+
+    //공지사항
+    $('.pop').click(function(e){
+        e.preventDefault();
+        $('.popup').fadeIn(500);
+    });
+    $('.close').click(function(){
+        $('.popup').fadeOut(500);
+    })
+
 }); //jquery
